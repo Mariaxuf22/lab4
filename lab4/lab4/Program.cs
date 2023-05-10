c9e9a023aef9eda490eb1cf38ccc617c4b14657b
Mariaxuf22 <97722846+Mariaxuf22@users.noreply.github.com>
2023-05-10 11:02:33 +0300

    Add files via upload

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 7, 3, 4, 9, 2, 5 };
+            int[] sortedOddArr = arr.Where(x => x % 2 != 0).OrderByDescending(x => x).ToArray();
+        }
+    }
+}
